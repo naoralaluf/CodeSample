7431417c2556a3aab2307c052bf9f85346dd3793
naoralaluf <naoralaluf@gmail.com>
2022-07-07 23:18:30 +0300

    Upload main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+
+class Point:
+    def __init__(self):
+        self.__x = 0
+        self.__y = 0
+    
+    def __str__(self):
+        return f"({self.x},{self.y})"
+    
+    def get_x(self):
+        return self.__x
+    
+    def get_y(self):
+        return self.__y
+    
+    def set_x(self, x):
+        self.__x = x
+    
+    def set_y(self, y):
+        self.__y = y
+    
+    @property
+    def x(self):
+        return self.get_x()
+    
+    @property
+    def y(self):
+        return self.get_y()
+    
+    @x.setter
+    def x(self, x):
+        self.set_x(x)
+    
+    @y.setter
+    def y(self, y):
+        self.set_y(y)
+    
